915285fea2f0f31b43ad631c461bc5b555c66575
Beng Tan <bengtan@users.noreply.github.com>
2020-12-22 14:27:14 +0800

    PWA experimentation.

---
+++ webapp/service-worker.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2020-present Beng Tan
+ */
+
+const CACHE_PREFIX = 'main.'
+//
+const CACHE_VERSION = "20201222" + '.' + "7432aae"  // + '-1'
+const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION
+
+const filesToCache = [
+  './',
+  '/favicon.ico',
+  // 'index.html',
+  'Loading_indicator.gif',
+  'logo-192.png',
+  'logo-512.png',
+  'main.js',
+  // 'main.js.map',
+  'manifest.webmanifest',
+  'rc-menu/index.css',
+  'sql-wasm-debug.wasm',
+  'style.css',
+]
+
+self.addEventListener('install', event => {
+  // Cache required files
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(filesToCache)
+    })
+  )
+
+  // Force service worker to change
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  // Delete old caches
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName.startsWith(CACHE_PREFIX) && cacheName != CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        // console.log(`Found ${event.request.url} in cache`)
+        return response
+      }
+
+      // console.log(`Network fetch for ${event.request.url}`)
+      return fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request.url, response.clone())
+          return response
+        })
+      })
+    }).catch(e => {
+      // Do nothing
+    })
+  )
+})
