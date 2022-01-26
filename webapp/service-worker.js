9ec7658c51a57b168128f2587705961fce7397d5
Beng Tan <bengtan@users.noreply.github.com>
2022-01-26 10:29:12 +0800

    Import web edition @a56e3999 (core 1.0.6@32d34898)

--- webapp/service-worker.js
+++ webapp/service-worker.js
@@ -4,7 +4,7 @@
 
 const CACHE_PREFIX = 'main.'
 //
-const CACHE_VERSION = "20201231" + '.' + "98e0b1a"  // + '-1'
+const CACHE_VERSION = "20220126" + '.' + "a56e399"  // + '-1'
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION
 
 const filesToCache = [
